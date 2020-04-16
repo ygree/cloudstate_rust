8ecbb318ec707a1235991b5ee544d2fe66dfc133
Yury Gribkov <yury.gribkov@gmail.com>
2020-04-15 22:22:53 -0400

    Use tonic::include_proto! to import generated Rust code

--- protocols/build.rs
+++ protocols/build.rs
@@ -5,11 +5,9 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .out_dir("src")
-        // .out_dir("src")
+        // .out_dir("src") // Uncomment if need to take a look at the sources. Use default output to be able use include_proto! macro.
         .compile(&[
             "protocol/cloudstate/event_sourced.proto",
-
         ], &[
             "protocol",
         ])
