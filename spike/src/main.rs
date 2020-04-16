48109c8a9fcb9e5c3e5c2be9016c8c163b08ab88
Yury Gribkov <yury.gribkov@gmail.com>
2020-04-15 22:23:19 -0400

    Unimplemented EventSourcedServerImpl

--- spike/src/main.rs
+++ spike/src/main.rs
@@ -2,17 +2,24 @@
 
 use std::convert::Infallible;
 
-use protocols::cloudstate::eventsourced::event_sourced_server::EventSourced;
+use protocols::cloudstate::eventsourced::event_sourced_server::{EventSourced, EventSourcedServer};
 use protocols::cloudstate::eventsourced::{EventSourcedStreamIn, EventSourcedStreamOut};
 use tonic::{Status, Streaming, Response, Request};
+use tonic::transport::Server;
+use std::pin::Pin;
+use futures_core::Stream;
 
-struct MyEventSourced;
 
-impl EventSourced for MyEventSourced {
-    type handleStream = ();
+#[derive(Default)]
+struct EventSourcedServerImpl;
+
+#[tonic::async_trait]
+impl EventSourced for EventSourcedServerImpl {
+    type handleStream = Pin<Box<dyn Stream<Item = Result<EventSourcedStreamOut, Status>> + Send + Sync>>;
+
 
     async fn handle(&self, request: Request<Streaming<EventSourcedStreamIn>>) -> Result<Response<Self::handleStream>, Status> {
-        unimplemented!()
+        Err(Status::unimplemented("not implemented"))
     }
 }
 
@@ -142,7 +149,7 @@ impl ShopCart {
     }
 }
 
-fn main() {
+fn fun() {
     let cart = Cart {
         items: vec![LineItem {
             product_id: "1234".to_string(),
@@ -159,3 +166,16 @@ fn main() {
 
     // entity.add_item()
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:9000".parse().unwrap();
+    let server = EventSourcedServerImpl::default();
+
+    let svc = EventSourcedServer::new(server);
+
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    Ok(())
+}
+
