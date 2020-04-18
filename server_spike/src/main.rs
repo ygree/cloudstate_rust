b1bc5897efa4309b72f48a8f78f100741033655d
Yury Gribkov <yury.gribkov@gmail.com>
2020-04-18 00:01:49 -0400

    Fix try_stream compile error

--- server_spike/src/main.rs
+++ server_spike/src/main.rs
@@ -1,39 +1,50 @@
 
 use protocols::cloudstate::eventsourced::event_sourced_server::{EventSourced, EventSourcedServer};
 use protocols::cloudstate::eventsourced::{EventSourcedStreamIn, EventSourcedStreamOut};
-use protocols::cloudstate::eventsourced::event_sourced_stream_out;
+use protocols::cloudstate::eventsourced::{event_sourced_stream_in, event_sourced_stream_out};
 use tonic::{Status, Streaming, Response, Request};
 use tonic::transport::Server;
 use std::pin::Pin;
-use futures_core::Stream;
+// use futures_core::Stream; // TODO: it caused compile issues
+use futures::Stream;
 
 #[derive(Default)]
 struct EventSourcedServerImpl;
 
 #[tonic::async_trait]
 impl EventSourced for EventSourcedServerImpl {
-    type handleStream = Pin<Box<dyn Stream<Item = Result<EventSourcedStreamOut, Status>> + Send + Sync>>;
+    type handleStream = Pin<Box<dyn Stream<Item = Result<EventSourcedStreamOut, Status>> + Send + Sync + 'static>>;
 
     //TODO https://github.com/hyperium/tonic/blob/master/examples/routeguide-tutorial.md#bidirectional-streaming-rpc
 
-    async fn handle(&self, request: Request<Streaming<EventSourcedStreamIn>>) -> Result<Response<Self::handleStream>, Status> {
+    async fn handle(&self, request: Request<tonic::Streaming<EventSourcedStreamIn>>) -> Result<Response<Self::handleStream>, Status> {
         let mut stream = request.into_inner();
 
-        // let output = async_stream::try_stream! {
-        //     while let Some(message) = stream.next().await {
-        //         //TODO
-        //         yield
-        //     }
-        // };
-        //
-        // Ok(Response::new(Box::pin(output)
-        //     as Pin<Box<dyn Stream<Item = Result<EventSourcedStreamOut, Status>> + Send + Sync + 'static>,
-        // >))
+        //         // match message {
+        //         //     event_sourced_stream_in::Message::Init(init) => (),
+        //         //     event_sourced_stream_in::Message::Event(init) => (),
+        //         //     event_sourced_stream_in::Message::Command(init) => (),
+        //         // }
+        // // //         //TODO
+        //         let msg = EventSourcedStreamOut {
+        //             message: None,
+        //         }
+        //         yield Response::new(msg);
 
-        // event_sourced_stream_out::Message::Reply
+        //message: event_sourced_stream_in::Message
 
-        //EventSourcedStreamIn.message.
-        Err(Status::unimplemented("not implemented"))
+        let output = async_stream::try_stream! {
+            while let Some(message) = stream.message().await? {
+            // while let Some(message) = stream.next().await {
+                let msg = EventSourcedStreamOut {
+                    message: None,
+                };
+                yield msg;
+            }
+        };
+
+        Ok(Response::new(Box::pin(output) as Self::handleStream))
+        // Err(Status::unimplemented("not implemented"))
     }
 }
 
