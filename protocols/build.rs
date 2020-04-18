e7cd286321659d4e882527916545d1fca1d8e64a
Yury Gribkov <yury.gribkov@gmail.com>
2020-04-18 16:15:10 -0400

    Generate client code

--- protocols/build.rs
+++ protocols/build.rs
@@ -4,8 +4,9 @@ fn main() {
 
     tonic_build::configure()
         .build_server(true)
-        .build_client(false)
+        .build_client(true)
         // .out_dir("src") // Uncomment if need to take a look at the sources. Use default output to be able use include_proto! macro.
+        // NOTE: when out_dir is specified then `tonic::include_proto!` won't be able to locate generated file
         .compile(&[
             "protocol/cloudstate/event_sourced.proto",
         ], &[
@@ -15,8 +16,9 @@ fn main() {
 
     tonic_build::configure()
         .build_server(true)
-        .build_client(false)
-        .out_dir("src") // Uncomment if need to take a look at the sources. Use default output to be able use include_proto! macro.
+        .build_client(true)
+        // .out_dir("src") // Uncomment if need to take a look at the sources. Use default output to be able use include_proto! macro.
+        // NOTE: when out_dir is specified then `tonic::include_proto!` won't be able to locate generated file
         .compile(&[
             "example/shoppingcart/persistence/domain.proto",
         ], &[
