e35ee2e599b117c6d7fb4f29c5043453a9a92c5c
Yury Gribkov <yury.gribkov@gmail.com>
2020-05-04 22:22:31 -0400

    Spike alternative approach for ShoppingCartEntity

---
+++ server_spike/examples/shopping_cart2.rs
@@ -0,0 +1,35 @@
+
+use protocols::example::shoppingcart::{
+    AddLineItem, RemoveLineItem, GetShoppingCart, Cart,
+    persistence::{self, ItemAdded, ItemRemoved, LineItem}
+};
+
+pub struct ShoppingCartEntity(Cart);
+
+trait Context {
+    fn emit<T>(event: T);
+    fn fail(message: String);
+}
+
+//TODO Spike a possible alternative entity interface implementation
+impl ShoppingCartEntity {
+    // it need to be checked with a macro whether all the command or event handlers are implemented
+    fn add_line_item(&self, cmd: AddLineItem, cx: &mut impl Context) {}
+    fn remove_line_item(&self, cmd: RemoveLineItem, cx: &mut impl Context) {}
+    fn get_shopping_cart(&self, cmd: GetShoppingCart, cx: &mut impl Context) -> Cart {}
+    fn item_added(&mut self, evt: ItemAdded) {}
+    fn item_removed(&mut self, evt: ItemRemoved) {}
+    fn restore(&mut self, cart: persistence::Cart) {}
+    fn snapshot(&self) -> persistence::Cart {
+        let items = self.0.items.iter().map_into( |item|
+            persistence::LineItem {
+                product_id: item.product_id,
+                name: item.name,
+                quantity: item.quantity,
+            }
+        ).collect();
+        persistence::Cart {
+            items,
+        }
+    }
+}
