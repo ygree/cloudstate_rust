cf48c109356f904d3ea8f80c1bf347fce711b7cd
Yury Gribkov <yury.gribkov@gmail.com>
2020-06-13 14:30:51 -0400

    Get rid of protobuf derive module

--- cloudstate-protobuf-derive/tests/all.rs
+++
@@ -1,9 +0,0 @@
-
-#[test]
-fn tests() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/shopping_cart_test.rs");
-    t.compile_fail("tests/missing_package_attr.rs");
-    t.compile_fail("tests/incorrect_package_attribute.rs");
-    t.compile_fail("tests/package_attribute_without_value.rs");
-}
